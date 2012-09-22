a874201cb980da84e09f1339473ed2959ac3f60b
EternalFaith- <jyna.xii@gmail.com>
2012-09-22 22:55:26 +0000

    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@18 5d30610d-6562-4147-a3b2-4cfa0469b359

--- Train_Track_Puzzle/repository_test/Jya43.java
+++ Train_Track_Puzzle/repository_test/Jya43.java
@@ -16,5 +16,6 @@ public class Jya43 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is JJ.");
+		System.out.println("CMPT 373 - Software Development Methods");
 	}
 }
