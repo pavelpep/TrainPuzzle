8be6876c52631523f3907f9ddcfc25710faa9fa0
EternalFaith- <jyna.xii@gmail.com>
2012-09-22 22:45:36 +0000

    Added by JJ.
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@16 5d30610d-6562-4147-a3b2-4cfa0469b359

---
+++ Train_Track_Puzzle/repository_test/Jya43.java
@@ -0,0 +1,20 @@
+package repository_test;
+
+/**
+ * 
+ * @author $Author$
+ * @version $Revision$
+ * @since $Date$
+ * 
+ */
+
+public class Jya43 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is JJ.");
+	}
+}
