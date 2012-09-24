9713d998e2493962a55a2114c8e787ceab4e7048
pavelpep <p.pepeldjiyski@gmail.com>
2012-09-24 19:50:00 +0000

    Testing Pavel Pepeldjiyski pdp@sfu.ca
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@24 5d30610d-6562-4147-a3b2-4cfa0469b359

---
+++ TrainPuzzle/src/main/java/repository_test/Pavel.java
@@ -0,0 +1,17 @@
+
+public class Pavel {
+
+	private String name = "Pavel";
+	private Int studentId = 301067272;
+
+	public String getName() 
+	{
+		return name;
+	}
+	
+	public String getStudentId() 
+	{
+		return studentId;
+	}
+	
+}
