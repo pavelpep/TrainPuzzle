64cf443bd71e1d85079685bf615aa09a970c7400
pavelpep <p.pepeldjiyski@gmail.com>
2012-09-25 23:20:02 +0000

    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@40 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/simulator/Simulator.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/simulator/Simulator.java
@@ -5,10 +5,21 @@ public class Simulator {
 
 private int status;  // 0 = stop ; 1= go ; 2 = pause
 	
-	
+boolean isVictory;
+//private nexTrack;
+
+	public void Pause()
+	{
+		
+	}
+
 	public void Run() 
 	{
 		
 	}
 	
+	private void Stop()
+	{
+		
+	}
 }
