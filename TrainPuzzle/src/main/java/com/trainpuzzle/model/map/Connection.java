6eacae55b788e901f1733c440c75c33e6c07ed19
pavelpep <p.pepeldjiyski@gmail.com>
2012-09-27 23:40:08 +0000

    created connection object that will be used inside of track
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@54 5d30610d-6562-4147-a3b2-4cfa0469b359

---
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Connection.java
@@ -0,0 +1,14 @@
+package com.trainpuzzle.model.map;
+
+public class Connection {
+
+public int heading1;
+public	int heading2;
+	
+	public Connection(int entryHeading, int exitHeading ){
+		
+		heading1= entryHeading;
+		heading2 = exitHeading;
+	}
+
+}
