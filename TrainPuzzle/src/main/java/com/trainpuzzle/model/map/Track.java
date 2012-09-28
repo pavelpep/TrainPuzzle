0731447b95439c2048960fd88cc312cdcfd7890e
pavelpep <p.pepeldjiyski@gmail.com>
2012-09-28 23:32:28 +0000

    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@63 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Track.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Track.java
@@ -7,26 +7,17 @@ import java.util.Set;
 public class Track {
 
 	
-/*
-public final int NORTH_WEST = 0;  //   0       1       2
-public final int NORTH = 1;       //    +------*------+            
-public final int NORTH_EAST = 2;  //    |             |
-public final int EAST = 3;        //    |             |
-public final int SOUTH_EAST = 4;  //  7 *     TILE    * 3
-public final int SOUTH = 5;       //    |             |
-public final int SOUTH_WEST = 6;  //    |             |
-public final int WEST = 7;		  //    +------*------+
-*/						  //   6       5       4
+				          
 	public enum Heading {
-		NORTHWEST(0), 
-		NORTH(1), 
-		NORTHEAST(2), 
-		EAST(3), 
-		SOUTHEAST(4), 
-		SOUTH(5), 
-		SOUTHWEST(6), 
-		WEST(7);
-		
+		NORTHWEST(0), 				//   0       1       2
+		NORTH(1), 					//    +------*------+   
+		NORTHEAST(2),  				//    |             |
+		EAST(3),      				//    |             |
+		SOUTHEAST(4), 				//  7 *     TILE    * 3
+		SOUTH(5),					//    |             |
+		SOUTHWEST(6),				//    |             |
+		WEST(7);					//    +------*------+
+									//   6       5       4
 		private int headingValue;
 		
 		private Heading(int headingValue) {
