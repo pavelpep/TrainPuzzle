fe83c2bec6f5caa1ffab22f6cc1224085a768258
rmkbow <ronald.bow@gmail.com>
2012-09-28 02:28:11 +0000

    added my own track object code as comment underneath. I think we're gettign mixed up what we're doing
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@57 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Track.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Track.java
@@ -23,3 +23,55 @@ private List<Connection> connections;
 	}
 	
 }
+
+
+/*
+
+package com.trainpuzzle.model.map;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Track {
+	
+	//seems like making thisheading enum into a class of just finals ints would be better
+	public enum Heading {
+		NORTHWEST(0), 
+		NORTH(1), 
+		NORTHEAST(2), 
+		EAST(3), 
+		SOUTHEAST(4), 
+		SOUTH(5), 
+		SOUTHWEST(6), 
+		WEST(7);
+		
+		private int headingValue;
+		
+		private Heading(int headingValue) {
+			this.headingValue = headingValue;
+		}
+		
+		public int getValue() {
+			return headingValue;
+		}
+	}
+
+private Set<Connection> connections;
+
+	public Track() 
+	{
+		connections = new HashSet<Connection>();
+		//System.out.println();
+		
+	}
+	
+	public void addConnection(){
+		connections.add(new Connection(Heading.NORTH, Heading.SOUTH));
+		return;
+	}
+	
+	
+}
+
+*/
