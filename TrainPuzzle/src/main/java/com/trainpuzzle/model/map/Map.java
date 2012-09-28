4efb40542d82da9344260e74d4850740defe02d1
pavelpep <p.pepeldjiyski@gmail.com>
2012-09-28 23:26:39 +0000

    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@62 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Map.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Map.java
@@ -18,6 +18,21 @@ private Tile[][] tiles;
 	public Map() 
 	{
 		tiles = new Tile[60][40];
+		initializeTiles();
+	}
+	
+	private void initializeTiles(){
+		
+		int mapWidth = tiles.length;
+		int mapHeight = tiles[1].length;
+		for(int i=0; i < mapWidth; i++){
+			for(int j=0; j < mapWidth; j++){
+				tiles[mapWidth][mapHeight] = new Tile();
+				
+			}
+			
+		}
+		
 	}
 	
 }
