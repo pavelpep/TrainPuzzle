9814251d0cf91fc0e0cb0333f3696d0d05562c6c
EternalFaith- <jyna.xii@gmail.com>
2012-09-29 22:11:48 +0000

    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@74 5d30610d-6562-4147-a3b2-4cfa0469b359

---
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Landscape.java
@@ -0,0 +1,16 @@
+package com.trainpuzzle.model.map;
+
+/**
+ * Represents a tile that can have {@link Track} pieces placed on it
+ * 
+ * @author $Author$
+ * @version $Revision$
+ * @since $Date$
+ *
+ */
+public abstract class Landscape {
+	
+	public Landscape() {
+		
+	}
+}
