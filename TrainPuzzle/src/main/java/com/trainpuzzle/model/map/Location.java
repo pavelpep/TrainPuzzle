07a95ae3a2921a5f243e08a979937760614c42f4
EternalFaith- <jyna.xii@gmail.com>
2012-09-29 22:51:33 +0000

    Created Location.java
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@79 5d30610d-6562-4147-a3b2-4cfa0469b359

---
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Location.java
@@ -0,0 +1,13 @@
+package com.trainpuzzle.model.map;
+
+/**
+ * 
+ * @author $Author: jya43 $
+ * @version $Revision: 65 $
+ * @since $Date: 2012-09-28 16:45:18 -0700 (Fri, 28 Sep 2012) $
+ *
+ */
+
+public class Location {
+
+}
