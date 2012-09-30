7e8fe5cbff7c3b304c2560650187eae7e279bd16
shannaw <shanna.walters@gmail.com>
2012-09-30 22:06:48 +0000

    Added parameter "gridHeight" to initializeComponent method to further define the jComponents (buttons, labels) it initializes.
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@94 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/ui/windows/MainMenu.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/ui/windows/MainMenu.java
@@ -25,12 +25,13 @@ class MainMenu extends Window implements ActionListener {
 	}
 
 	//initializes a specific jComponent
-	private void initializeComponent(JComponent jComponent, int fontLayout, int fontSize, Color bgColor, int gridX, int gridY, int gridWidth, int anchor, int fill, Insets inset, boolean isEnabled){
+	private void initializeComponent(JComponent jComponent, int fontLayout, int fontSize, Color bgColor, int gridX, int gridY, int gridWidth, int gridHeight, int anchor, int fill, Insets inset, boolean isEnabled){
 		jComponent.setFont(new Font("Arial", fontLayout, fontSize));
 		jComponent.setBackground(bgColor);
 		this.gbConstraints.gridx = gridX;
 		this.gbConstraints.gridy = gridY;
 		this.gbConstraints.gridwidth = gridWidth;
+		this.gbConstraints.gridheight = gridHeight;
 		this.gbConstraints.anchor = anchor;
 		this.gbConstraints.fill = fill;
 		this.gbConstraints.insets = inset;
@@ -40,27 +41,27 @@ class MainMenu extends Window implements ActionListener {
 	public void Create() {	    
 		
 		//Game Title
-		this.initializeComponent(this.menuLabel, Font.CENTER_BASELINE, 28, Color.BLACK, 0, 0, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10, 10, 0, 10), true);
+		this.initializeComponent(this.menuLabel, Font.CENTER_BASELINE, 28, Color.BLACK, 0, 0, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10, 10, 0, 10), true);
 		this.menuLabel.setText("Train Track Puzzle Game");
 		this.add(this.menuLabel, this.gbConstraints);
 		
 		//Continue Button
-		this.initializeComponent(this.continueButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.ORANGE, 0, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(30, 0, 10, 0), true);
+		this.initializeComponent(this.continueButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.ORANGE, 0, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(30, 0, 10, 0), true);
 		this.continueButton.setText("Continue Campaign");
 		this.add(this.continueButton, this.gbConstraints);
 	
 		//Profiles Button
-		this.initializeComponent(this.profilesButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.ORANGE, 0, 2, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(10, 0, 10, 0), true);
+		this.initializeComponent(this.profilesButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.ORANGE, 0, 2, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(10, 0, 10, 0), true);
 		this.profilesButton.setText("Add/Change User");
 		this.add(this.profilesButton, this.gbConstraints);
 		
 		//Credits Button
-		this.initializeComponent(this.creditsButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.ORANGE, 0, 3, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(10, 0, 10, 0), true);
+		this.initializeComponent(this.creditsButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.ORANGE, 0, 3, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(10, 0, 10, 0), true);
 		this.creditsButton.setText("Credits");
 		this.add(this.creditsButton, this.gbConstraints);
 		
 		//Exit Button
-		this.initializeComponent(this.exitButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.LIGHT_GRAY, 0, 4, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(40, 0, 20, 0), true);
+		this.initializeComponent(this.exitButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.LIGHT_GRAY, 0, 4, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(40, 0, 20, 0), true);
 		this.exitButton.setText("Exit");
 		this.add(this.exitButton, this.gbConstraints);
 
