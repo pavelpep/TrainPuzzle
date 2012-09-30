bafbd8c322b94551e018543d51d7a0028780b602
shannaw <shanna.walters@gmail.com>
2012-09-30 20:30:41 +0000

    Removed duplicate code, most notably by creating a method to initialize JComponents (Labels, Buttons) rather than initializing each component individually.
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@86 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/ui/windows/MainMenu.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/ui/windows/MainMenu.java
@@ -3,127 +3,92 @@ package com.trainpuzzle.ui.windows;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.util.*;
 
 // The main menu for the Train Track Puzzle game
 class MainMenu extends Window implements ActionListener {
 	// Layout Manager
-	private GridBagConstraints c;
+	private GridBagConstraints gbConstraints = new GridBagConstraints();
 	
 	// Window elements
-	private JLabel menuLabel;
-	private JButton continueButton;
-	private JButton profilesButton; 
-	private JButton creditsButton;
-	private JButton exitButton;
+	private JLabel menuLabel = new JLabel();
+	private JButton continueButton = new JButton();
+	private JButton profilesButton = new JButton(); 
+	private JButton creditsButton = new JButton();
+	private JButton exitButton = new JButton();
 	
 	// Constructor
-	public MainMenu() {
-		menuLabel = null;
-		continueButton = null;
-		profilesButton = null;
-		creditsButton = null;
-		exitButton = null;
-		
-		c = new GridBagConstraints();
+	public MainMenu() {		
 		setLayout(new GridBagLayout());
 		setSize(new Dimension(640,480));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLocationRelativeTo(null);	
+		setLocationRelativeTo(null);		
+	}
+
+	//initializes a specific jComponent
+	private void initializeComponent(JComponent jComponent, int fontLayout, int fontSize, Color bgColor, int gridX, int gridY, int gridWidth, int anchor, int fill, Insets inset, boolean isEnabled){
+		jComponent.setFont(new Font("Arial", fontLayout, fontSize));
+		jComponent.setBackground(bgColor);
+		this.gbConstraints.gridx = gridX;
+		this.gbConstraints.gridy = gridY;
+		this.gbConstraints.gridwidth = gridWidth;
+		this.gbConstraints.anchor = anchor;
+		this.gbConstraints.fill = fill;
+		this.gbConstraints.insets = inset;
+		jComponent.setEnabled(isEnabled);
 	}
 	
 	public void Create() {	    
-		// Game title
-		menuLabel = new JLabel("Train Track Puzzle Game");
-		menuLabel.setFont(new Font("Arial", Font.CENTER_BASELINE, 28));
-		menuLabel.setForeground(Color.black);
-		c.gridx = 0;
-		c.gridy = 0;
-		c.gridwidth = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		c.fill = GridBagConstraints.NONE;
-		c.insets = new Insets(10, 10, 0, 10);
-		this.add(menuLabel, c);
 		
-		// Continue Button
-		continueButton = new JButton("Continue Campaign");
-		continueButton.setFont(new Font("Arial", Font.LAYOUT_LEFT_TO_RIGHT, 20));
-		continueButton.setBackground(Color.ORANGE);
-		c.gridx = 0;
-		c.gridy = 1;
-		c.gridwidth = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.insets = new Insets(30, 0, 10, 0);
-		continueButton.setEnabled(true);
-		this.add(continueButton, c);
+		this.initializeComponent(this.menuLabel, Font.CENTER_BASELINE, 28, Color.BLACK, 0, 0, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10, 10, 0, 10), true);
+		this.menuLabel.setText("Train Track Puzzle Game");
+		this.add(this.menuLabel, this.gbConstraints);
 		
-		// Profile Button
-		profilesButton = new JButton("Add/Change User");
-		profilesButton.setFont(new Font("Arial", Font.LAYOUT_LEFT_TO_RIGHT, 20));
-		profilesButton.setBackground(Color.ORANGE);
-		c.gridx = 0;
-		c.gridy = 2;
-		c.gridwidth = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.insets = new Insets(10, 0, 10, 0);
-		profilesButton.setEnabled(false);
-		this.add(profilesButton, c);
+		this.initializeComponent(this.continueButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.ORANGE, 0, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(30, 0, 10, 0), true);
+		this.continueButton.setText("Continue Campaign");
+		this.add(this.continueButton, this.gbConstraints);
+	
+		this.initializeComponent(this.profilesButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.ORANGE, 0, 2, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(10, 0, 10, 0), true);
+		this.profilesButton.setText("Add/Change User");
+		this.add(this.profilesButton, this.gbConstraints);
 		
-		// Credits Button
-		creditsButton = new JButton("Credits");
-		creditsButton.setFont(new Font("Arial", Font.LAYOUT_LEFT_TO_RIGHT, 20));	
-		creditsButton.setBackground(Color.ORANGE);
-		c.gridx = 0;
-		c.gridy = 3;
-		c.gridwidth = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.insets = new Insets(10, 0, 10, 0);
-		creditsButton.setEnabled(false);
-		this.add(creditsButton, c);
+		this.initializeComponent(this.creditsButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.ORANGE, 0, 3, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(10, 0, 10, 0), true);
+		this.creditsButton.setText("Credits");
+		this.add(this.creditsButton, this.gbConstraints);
 		
-		// Exit Button
-		exitButton = new JButton("Exit");
-		exitButton.setFont(new Font("Arial", Font.LAYOUT_LEFT_TO_RIGHT, 20));	
-		c.gridx = 0;
-		c.gridy = 4;
-		c.gridwidth = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		c.fill = GridBagConstraints.NONE;
-		c.insets = new Insets(40, 0, 20, 0);
-		this.add(exitButton, c);		
-		
+		this.initializeComponent(this.exitButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.LIGHT_GRAY, 0, 4, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(40, 0, 20, 0), true);
+		this.exitButton.setText("Exit");
+		this.add(this.exitButton, this.gbConstraints);
+
 		//this.pack();
 		this.setVisible(true);
 		
 		// ActionListener for window elements
-		continueButton.setActionCommand("continue");
-		continueButton.addActionListener(this);
+		this.continueButton.setActionCommand("continue");
+		this.continueButton.addActionListener(this);
 		
-		exitButton.setActionCommand("exit");
-		exitButton.addActionListener(this);
-		
-		profilesButton.setActionCommand("profiles");
-		profilesButton.addActionListener(this);
+		this.profilesButton.setActionCommand("profiles");
+		this.profilesButton.addActionListener(this);
 				
+		this.exitButton.setActionCommand("exit");
+		this.exitButton.addActionListener(this);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
 		// Loads the level select menu
-		if (e.getActionCommand() == "continue") {
-				WindowManager.getManager().setActiveWindow(new LevelSelect()); 
-				WindowManager.getManager().updateWindows();
-			}
-		// Exit Program
-		if (e.getActionCommand() == "exit") {
-			System.exit(0);
+		String action = e.getActionCommand();
+		//if (e.getActionCommand() == "continue") {
+		if (action.equals("continue")) {
+			WindowManager.getManager().setActiveWindow(new LevelSelect()); 
+			WindowManager.getManager().updateWindows();
 		}
-		if (e.getActionCommand() == "profiles") {
+		if (action.equals("profiles")) {
 			WindowManager.getManager().setActiveWindow(new Profiles()); 
 			WindowManager.getManager().updateWindows();
 		}			
+		// Exit Program
+		if (action.equals("exit")) {
+			System.exit(0);
+		}
 	}
 	
 }
