41c47b0790def6471231676cbc121b317d1bc247
rmkbow <ronald.bow@gmail.com>
2012-09-30 20:51:44 +0000

    rotateTrack method
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@87 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Track.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Track.java
@@ -10,10 +10,7 @@ import java.util.Set;
  * @since $Date$
  */
 public class Track {
-
 	
-	
-				          
 	public enum Heading {
 		NORTHWEST(0), 				//   0       1       2
 		NORTH(1), 					//    +------*------+   
@@ -31,37 +28,41 @@ public class Track {
 			this.headingValue = headingValue;
 		}
 		
+		public static Heading getHeading(int headingValue) {
+			Heading headingResult = null;
+			switch (headingValue) {
+				case 0:
+					headingResult = NORTHWEST;
+					break;
+				case 1:
+					headingResult = NORTH;
+					break;
+				case 2:
+					headingResult = NORTHEAST;
+					break;
+				case 3:
+					headingResult = EAST;
+					break;
+				case 4:
+					headingResult = SOUTHEAST;
+					break;
+				case 5:
+					headingResult = SOUTH;
+					break;
+				case 6:
+					headingResult = SOUTHWEST;
+					break;
+				case 7:
+					headingResult = WEST;
+					break;
+			}
+			return headingResult;
+		}
+		
 		public Heading opposite() {
 			final int oppositeIncrement = 4;
 			int oppositeValue = (headingValue + oppositeIncrement) % numberOfHeadings;
-			Heading oppositeHeading = null;
-			switch (oppositeValue) {
-				case 0:
-					oppositeHeading = NORTHWEST;
-					break;
-				case 1:
-					oppositeHeading = NORTH;
-					break;
-				case 2:
-					oppositeHeading = NORTHEAST;
-					break;
-				case 3:
-					oppositeHeading = EAST;
-					break;
-				case 4:
-					oppositeHeading = SOUTHEAST;
-					break;
-				case 5:
-					oppositeHeading = SOUTH;
-					break;
-				case 6:
-					oppositeHeading = SOUTHWEST;
-					break;
-				case 7:
-					oppositeHeading = WEST;
-					break;
-			}
-			return oppositeHeading;
+			return getHeading(oppositeValue);
 		}
 		
 		public int getValue() {
@@ -70,7 +71,7 @@ public class Track {
 	}
 
 	private Set<Connection> connections;
-
+	
 	public Track() 
 	{
 		connections = new HashSet<Connection>();
@@ -87,8 +88,13 @@ public class Track {
 	}
 	
 	public void rotateTrack() {
+		final int rotateIncrement = 2;
 		for(Connection connection : connections){
-			connection.modifyConnection(connection.getHeadings()[0].opposite(), connection.getHeadings()[1].opposite());
+			int rotatedValue1 = (connection.getHeadingPair()[0].getValue() + rotateIncrement) % Track.Heading.numberOfHeadings;
+			int rotatedValue2 = (connection.getHeadingPair()[1].getValue() + rotateIncrement) % Track.Heading.numberOfHeadings;
+			Track.Heading rotatedHeading1 = Track.Heading.getHeading(rotatedValue1);
+			Track.Heading rotatedHeading2 = Track.Heading.getHeading(rotatedValue2);
+			connection.modifyConnection(rotatedHeading1,rotatedHeading2);
 		}
 		return;
 	}
