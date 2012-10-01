b962223acda845b4c8c879dc356358ed063d9be4
dehud93 <hdeng@sfu.ca>
2012-10-01 00:45:44 +0000

    added level class
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@122 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/model/level/Condition.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/level/Condition.java
@@ -1,5 +1,16 @@
 package com.trainpuzzle.model.level;
 
 public class Condition {
-
+	private int latitude;
+	private int longitude;
+	public void setLocation(int[] location){
+		this.latitude = location[0];
+		this.longitude = location[1];
+	}
+	public int[] getLocation(){
+		int[] location = new int[2];
+		location[0] = this.latitude;
+		location[1] = this.longitude;
+		return location;
+	}
 }
