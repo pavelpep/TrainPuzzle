f3f2473c6c69906b586cd76c77c419f9fa5f7caf
newmanlai <newmanlai.lm@gmail.com>
2012-10-01 00:38:09 +0000

    Added functions in LevelLoader
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@119 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/controller/LevelLoader.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/controller/LevelLoader.java
@@ -1,9 +1,45 @@
 package com.trainpuzzle.controller;
+
+import com.trainpuzzle.model.map.*;
+import com.trainpuzzle.controller.Level;
+
 /**
  * use for selecting the a level.
  * @author huachuandeng
  *
  */
 public class LevelLoader {
-
+	
+	private Level level;
+	
+	LevelLoader(int levelNumber) {
+		level = new Level(levelNumber);
+		
+		if (!hasMap()) {
+			generateMap();
+		}
+	}
+	
+	public Map getLevel() {
+		return level;
+	}
+	
+	/**
+	 * Generate a random map
+	 */
+	public void generateMap() {
+		
+	}
+	
+	
+	/**
+	 * search for a possible way
+	 */
+	private boolean isValidMap() {
+		return true;
+	}
+	
+	private boolean hasMap() {
+		
+	}
 }
