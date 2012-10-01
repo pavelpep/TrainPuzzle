592e4b279168198324ce707b11e302cd2c92caba
0ldskool <j.chahal20@gmail.com>
2012-10-01 02:59:16 +0000

    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@138 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/controller/Application.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/controller/Application.java
@@ -1,15 +1,46 @@
 package com.trainpuzzle.controller;
 
+import com.trainpuzzle.model.level.Level;
+import com.trainpuzzle.model.map.Map;
+import com.trainpuzzle.model.map.Track;
+
+/**
+ * 
+ * @author $Author: jsc15 $
+ * @version $Revision: 85 $
+ * @since $Date: 2012-09-30 00:53:30 -0700 (Sun, 30 Sep 2012) $
+ */
 public class Application {
 	private LevelLoader levelLoader;
 	private TrackBuilder trackBuilder;
 	private Simulator simulator;
 	
+	private Level loadedLevel;
+	private Level modifiedLevel;
+	
 	public Application(int levelNumber) {
 		//todo change int to enum?
-		levelLoader = new LevelLoader(levelNumber);
-		trackBuilder = new TrackBuilder(levelLoader.getLevel());
+		levelLoader = new LevelLoader();
+		loadedLevel = levelLoader.loadLevel(levelNumber);
+		trackBuilder = new TrackBuilder(loadedLevel);
+		modifiedLevel = trackBuilder.getLevel();
 	}
 	
+	public void runSimulation() {
+		modifiedLevel = trackBuilder.getLevel();
+		simulator = new Simulator(modifiedLevel);
+	}
 	
+	public void placeTrack(Track track, int latitude, int longitude) {
+		trackBuilder.placeTrack(track, latitude, longitude);
+		modifiedLevel = trackBuilder.getLevel();
+	}
+	
+	public Map getBlankMap() {
+		return loadedLevel.getMap();
+	}
+	
+	public Map getTrackMap() {
+		return trackBuilder.getMap();
+	}
 }
