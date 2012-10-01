fa479643e6c61b98fd398c2ab2986c4b7453e66f
0ldskool <j.chahal20@gmail.com>
2012-10-01 02:58:19 +0000

    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@136 5d30610d-6562-4147-a3b2-4cfa0469b359

---
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Location.java
@@ -0,0 +1,28 @@
+package com.trainpuzzle.model.map;
+
+public class Location {
+	
+	private int latitude;
+	private int longitude;
+	
+	public Location(int latitude, int longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public int getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(int latitude) {
+		this.latitude = latitude;
+	}
+
+	public int getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(int longitude) {
+		this.longitude = longitude;
+	}
+}
