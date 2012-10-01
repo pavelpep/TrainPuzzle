4dfa9c5064f38e4e3bf047e51c09c085b22b84d7
EternalFaith- <jyna.xii@gmail.com>
2012-10-01 01:27:48 +0000

    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@130 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Train.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Train.java
@@ -7,17 +7,19 @@ package com.trainpuzzle.model.map;
  * @since $Date$
  *
  */
+
 public class Train {
  
 	private int[] location; 
 	private Track.Heading heading;
-	public Train() 
-	{
+	
+	public Train() {
 		this.location = new int[2];
 		this.location[0] = 0;
 		this.location[1] = 0;
 	}
-	public Train(int latitude, int longitude){
+	
+	public Train(int latitude, int longitude) {
 		this.location = new int[2];
 		this.location[0] = latitude;
 		this.location[1] = longitude;
@@ -25,18 +27,18 @@ public class Train {
 
 	public int[] getLocation() {
 		return location;
-		
 	}
 
 	public void setLocation(int latitude, int longitude ) {
 		this.location[0] = latitude;
 		this.location[1] = longitude;
 	}
+	
 	public Track.Heading getHeading() {
 		return heading;
 	}
+	
 	public void setHeading(Track.Heading heading) {
 		this.heading = heading;
 	}
-	
 }
