53fbbbc8f62355e268962cb56549effc31b9b82e
EternalFaith- <jyna.xii@gmail.com>
2012-10-02 22:46:21 +0000

    Modified Map class.
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@164 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Map.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/map/Map.java
@@ -17,8 +17,8 @@ public class Map {
 	private int mapHeight;
 	
 	public Map() {
-		mapWidth = 15;
-		mapHeight = 35;
+		mapWidth = 100;
+		mapHeight = 100;
 		tiles = new Tile[mapWidth][mapHeight];
 		initializeTiles();
 		addSomeWaterTiles(); // for testing
@@ -26,26 +26,28 @@ public class Map {
 	
 	private void initializeTiles() {
 			
-		for(int i=0; i < mapWidth; i++) {
-			for(int j=0; j < mapHeight; j++) {
-				tiles[i][j] = new Tile();
+		for(int w=0; w < mapWidth; w++) {
+			for(int h=0; h < mapHeight; h++) {
+				tiles[w][h] = new Tile();
 			}	
 		}
-		
 	}
+	
 	private void addSomeWaterTiles() {
 	 this.tiles[10][10].setLandscapeType("water");
 	 this.tiles[5][5].setLandscapeType("water");
-	 this.tiles[2][2].setLandscapeType("water");
+	 this.tiles[2][3].setLandscapeType("water");
 	}
 	
 	public Tile getTile(int latitude, int longitude) {
 		return tiles[latitude][longitude];
 	}
-	public int getMapWidth(){
+	
+	public int getMapWidth() {
 		return mapWidth;
 	}
-	public int getMapHeight(){
+	
+	public int getMapHeight() {
 		return mapHeight;
 	}
 	
