c11b7160759ed4c0519e4810bcaa06b37beb4d3f
0ldskool <j.chahal20@gmail.com>
2012-10-03 01:46:52 +0000

    Created a very basic Train Crash exception
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@181 5d30610d-6562-4147-a3b2-4cfa0469b359

---
+++ TrainPuzzle/src/main/java/com/trainpuzzle/exception/TrainCrashException.java
@@ -0,0 +1,12 @@
+package com.trainpuzzle.exception;
+
+public class TrainCrashException extends Exception {
+
+	public TrainCrashException() {
+	}
+	
+	public TrainCrashException(String message) {
+		super(message);
+	}
+	
+}
