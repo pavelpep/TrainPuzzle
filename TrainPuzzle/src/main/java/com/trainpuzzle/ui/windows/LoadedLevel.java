5d522a246e4e42ae2dceb1c1a097f3d63ee3aea0
rmkbow <ronald.bow@gmail.com>
2012-10-03 03:18:40 +0000

    redrawTrain method, ronald and JJ
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@189 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/ui/windows/LoadedLevel.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/ui/windows/LoadedLevel.java
@@ -6,9 +6,13 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
 
+import org.apache.log4j.Logger;
+
+import com.trainpuzzle.controller.Simulator;
 import com.trainpuzzle.model.level.Level;
 import com.trainpuzzle.model.map.Train;
 import com.trainpuzzle.model.map.Location;
+import com.trainpuzzle.model.map.Track;
 
 import java.util.*;
 
@@ -24,12 +28,15 @@ public class LoadedLevel extends Window implements ActionListener {
 	private JPanel toolbarPanel;
 	private JLabel grassTile;
 	private JLabel trackTile;
+	private Logger logger = Logger.getLogger(LoadedLevel.class);
 	
-	private Location previousTrainLocation;
+	private int previousTrainLatitude;
+	private int previousTrainLongitude;
 	
 	int mapWidth = 20;
 	int mapHeight = 15;
 	
+	private JLayeredPane mapTile;
 	private JLayeredPane[][] mapTiles;
 	
 	
@@ -45,7 +52,8 @@ public class LoadedLevel extends Window implements ActionListener {
 		mapPanel = null;
 		toolbarPanel = null;
 		
-		previousTrainLocation = new Location(0,0);
+		previousTrainLatitude = 0;
+		previousTrainLongitude = 0;
 		
 		c = new GridBagConstraints();
 		setLayout(new GridBagLayout());
@@ -56,21 +64,36 @@ public class LoadedLevel extends Window implements ActionListener {
 	
 	public void redrawTrain(Train train) {
 		
-		int previousLatitude = previousTrainLocation.getLatitude();
-		int previousLongitude = previousTrainLocation.getLongitude();
-		mapTiles[previousLatitude][previousLongitude].remove(2);
+		//System.out.println("old location @ " + previousTrainLatitude + ", " + previousTrainLongitude);
+		
+
 		
 		JLabel trainTile;
     	trainTile = new JLabel(new ImageIcon("src/main/resources/images/train.png"));
     	trainTile.setBounds(0,0,40,40);
         
 		Location trainLocation = train.getLocation();
+		
+		
 	
 		int latitude = trainLocation.getLatitude();
 		int longitude = trainLocation.getLongitude();
-		previousTrainLocation = trainLocation;
+		
 		
 		mapTiles[latitude][longitude].add(trainTile, new Integer(2));
+		
+		try{
+			mapTiles[previousTrainLatitude][previousTrainLongitude].remove(mapTiles[previousTrainLatitude][previousTrainLongitude].getComponentsInLayer(2)[0]);
+			//System.out.println("removing @ " + previousTrainLatitude + ", " + previousTrainLongitude);
+        	
+			
+		}catch(Exception e){
+			logger.error(e.getStackTrace());
+		}
+		
+		previousTrainLatitude = trainLocation.getLatitude();
+		previousTrainLongitude= trainLocation.getLongitude();
+		
 	}
 	
 	public void Create() {	    
@@ -97,7 +120,7 @@ public class LoadedLevel extends Window implements ActionListener {
         for(int y=0; y < mapHeight; y++){
             for(int x=0; x < mapWidth; x++){
             	
-            	JLayeredPane mapTile = new JLayeredPane();
+            	mapTile = new JLayeredPane();
             	mapTile.setPreferredSize(new Dimension(40, 40));
             	
             	if(testLevel.getMap().getTile(y,x).getLandscapeType() == "grass"){
@@ -121,12 +144,44 @@ public class LoadedLevel extends Window implements ActionListener {
             	mapPanel.add(mapTile);
             	mapTiles[x][y] = mapTile;
             }
-    }
+        }
 		c.gridx = 0;
 		c.gridy = 1;
 		c.anchor = GridBagConstraints.CENTER;
 		c.fill = GridBagConstraints.NONE;
 		this.add(mapPanel, c);
+		
+		
+		/*
+		Simulator testSim = new Simulator(testLevel);
+		testSim.getTrain().setLocation(0, 4);
+		
+		
+		Train testTrain = new Train();
+		testTrain.setLocation(0, 4);
+		testTrain.setCompassHeading(Track.CompassHeading.EAST);
+		 for(int y=0; y < mapHeight; y++){
+	            for(int x=0; x < mapWidth; x++){
+	            	if(testLevel.getMap().getTile(y,x).hasTrack()){
+	            		redrawTrain(testSim.getTrain());
+	            		testSim.proceedNextTile();
+	            		System.out.println("NEW location @ " + testSim.getTrain().getLocation().getLatitude() + ", " + testSim.getTrain().getLocation().getLongitude());
+	            	}
+	            }
+	        }
+	        
+	     */
+		/*
+	            for(int x=0; x < mapWidth - 1; x++){
+	            	if(testLevel.getMap().getTile(4,x).hasTrack()){
+	            		mapTiles[x][4].remove(mapTiles[x][4].getComponentsInLayer(2)[0]);
+	            		
+	            		System.out.println(x + ", " + 4);
+	            	}
+	            }
+		*/
+		 
+		 
 		
 		// Track Panel
 		toolbarPanel = new JPanel();
