c02e56b47ee5c3a8aaa4655ab25a03a13524a561
shannaw <shanna.walters@gmail.com>
2012-10-08 03:35:48 +0000

    Edited to use a class moved locally to class "Window" that it inherits from.
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@217 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/ui/windows/MainMenu.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/ui/windows/MainMenu.java
@@ -6,8 +6,6 @@ import javax.swing.*;
 
 // The main menu for the Train Track Puzzle game
 class MainMenu extends Window implements ActionListener {
-	// Layout Manager
-	private GridBagConstraints gbConstraints = new GridBagConstraints();
 	
 	// Window elements
 	private JLabel menuLabel = new JLabel();
@@ -24,45 +22,33 @@ class MainMenu extends Window implements ActionListener {
 		setLocationRelativeTo(null);		
 	}
 
-	//initializes a specific jComponent
-	private void initializeComponent(JComponent jComponent, int fontLayout, int fontSize, Color bgColor, int gridX, int gridY, int gridWidth, int gridHeight, int anchor, int fill, Insets inset, boolean isEnabled){
-		jComponent.setFont(new Font("Arial", fontLayout, fontSize));
-		jComponent.setBackground(bgColor);
-		this.gbConstraints.gridx = gridX;
-		this.gbConstraints.gridy = gridY;
-		this.gbConstraints.gridwidth = gridWidth;
-		this.gbConstraints.anchor = anchor;
-		this.gbConstraints.fill = fill;
-		this.gbConstraints.insets = inset;
-		jComponent.setEnabled(isEnabled);
-	}
 	
 	public void Create() {	    
 		
-		//Game Title
-		this.initializeComponent(this.menuLabel, Font.CENTER_BASELINE, 28, Color.BLACK, 0, 0, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10, 10, 0, 10), true);
+		// Game Title
+		initializeComponent(this.menuLabel, Font.CENTER_BASELINE, 28, Color.BLACK, 0, 0, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10, 10, 0, 10), true);
 		this.menuLabel.setText("Train Track Puzzle Game");
-		this.add(this.menuLabel, this.gbConstraints);
+		this.add(this.menuLabel, gbConstraints);
 		
-		//Continue Button
-		this.initializeComponent(this.continueButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.ORANGE, 0, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(30, 0, 10, 0), true);
+		// Continue Button
+		initializeComponent(this.continueButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.ORANGE, 0, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(30, 0, 10, 0), true);
 		this.continueButton.setText("Continue Campaign");
-		this.add(this.continueButton, this.gbConstraints);
+		this.add(this.continueButton, gbConstraints);
 	
-		///Campaigns Button
-		this.initializeComponent(this.profilesButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.ORANGE, 0, 2, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(10, 0, 10, 0), true);
+		/// Campaigns Button
+		initializeComponent(this.profilesButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.ORANGE, 0, 2, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(10, 0, 10, 0), true);
 		this.profilesButton.setText("Add/Change User");
-		this.add(this.profilesButton, this.gbConstraints);
-		
-		//Credits Button
-		this.initializeComponent(this.creditsButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.ORANGE, 0, 3, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(10, 0, 10, 0), false);
+		this.add(this.profilesButton, gbConstraints);
+			
+		// Credits Button
+		initializeComponent(this.creditsButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.ORANGE, 0, 3, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(10, 0, 10, 0), false);
 		this.creditsButton.setText("Credits");
-		this.add(this.creditsButton, this.gbConstraints);
+		this.add(this.creditsButton, gbConstraints);
 		
-		//Exit Button
-		this.initializeComponent(this.exitButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.LIGHT_GRAY, 0, 4, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(40, 0, 20, 0), true);
+		// Exit Button
+		initializeComponent(this.exitButton, Font.LAYOUT_LEFT_TO_RIGHT, 20, Color.LIGHT_GRAY, 0, 4, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(40, 0, 20, 0), true);
 		this.exitButton.setText("Exit");
-		this.add(this.exitButton, this.gbConstraints);
+		this.add(this.exitButton, gbConstraints);
 
 		//this.pack();
 		this.setVisible(true);
@@ -79,16 +65,16 @@ class MainMenu extends Window implements ActionListener {
 	}
 	
 	public void actionPerformed(ActionEvent event) {
-		if (event.getActionCommand() == "continue") {
+		String action = event.getActionCommand();
+		
+		if (action == "continue") {
 			WindowManager.getManager().setActiveWindow(new LevelSelect()); 
 			WindowManager.getManager().updateWindows();
-		}
-		if (event.getActionCommand() == "campaigns") {
+		} else if (action == "campaigns") {
 			WindowManager.getManager().setActiveWindow(new Campaigns()); 
 			WindowManager.getManager().updateWindows();
-		}
-		// Exit Program
-		if (event.getActionCommand() == "exit") {
+		} else if (action == "exit") {
+			// Exit program
 			System.exit(0);
 		}
 	}
