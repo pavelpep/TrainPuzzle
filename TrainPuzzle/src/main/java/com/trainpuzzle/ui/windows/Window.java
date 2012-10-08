46db8fc7f526a0beb4756c3c00a96d68b4abf7e1
shannaw <shanna.walters@gmail.com>
2012-10-08 03:34:20 +0000

    Moved method to initialize jcomponents into this class Window for its subclasses to use (the use for this method appears in multiple classes).  Reduces duplicate code.
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@216 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/ui/windows/Window.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/ui/windows/Window.java
@@ -1,6 +1,7 @@
 package com.trainpuzzle.ui.windows;
 
 import java.awt.*;
+
 import javax.swing.*;
 
 /* The purpose of this abstract class (i.e. superclass) is to put the common method name Create() 
@@ -8,10 +9,25 @@ import javax.swing.*;
  */
 
 abstract class Window extends JFrame {
-	Window(){
-		
-	}
+	// Layout Manager
+	protected GridBagConstraints gbConstraints = new GridBagConstraints();
+			
+	Window() {}
 	
 	// Creates the JFrame with elements
 	public abstract void Create();
+	
+	// Initializes a specific jComponent
+	protected void initializeComponent(JComponent jComponent, int fontLayout, int fontSize, Color bgColor,
+			int gridX, int gridY, int gridWidth, int gridHeight, int anchor, int fill, Insets inset, boolean isEnabled) {
+		jComponent.setFont(new Font("Arial", fontLayout, fontSize));
+		jComponent.setBackground(bgColor);
+		this.gbConstraints.gridx = gridX;
+		this.gbConstraints.gridy = gridY;
+		this.gbConstraints.gridwidth = gridWidth;
+		this.gbConstraints.anchor = anchor;
+		this.gbConstraints.fill = fill;
+		this.gbConstraints.insets = inset;
+		jComponent.setEnabled(isEnabled);
+	}
 }
