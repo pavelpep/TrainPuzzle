fd085ee4b6c34f85ed2e32dce5daf24dbd59106f
rmkbow <ronald.bow@gmail.com>
2012-10-16 23:46:09 +0000

    simplified addSomeWaterTiles and moved addSomeTrackTiles
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@264 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/model/level/Level.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/level/Level.java
@@ -27,24 +27,23 @@ public class Level {
 	private void createLevelOne() {
 		map = new Board();
 		addSomeWaterTiles();
+		addSomeTrackTiles();
 		startLocation = new Location(4,0);
 		endLocation = new Location(4,19);
 	}
 	
 	private void addSomeWaterTiles() {
-		Tile tileToAddWater;
 		
-		tileToAddWater = map.getTile(10, 10);
-		tileToAddWater.setLandscapeType("water");
-		map.setTile(tileToAddWater, 10, 10);
+		map.getTile(10, 10).setLandscapeType("water");
+		map.getTile(5, 5).setLandscapeType("water");
+		map.getTile(2, 3).setLandscapeType("water");
 		
-		tileToAddWater = map.getTile(5, 5);
-		tileToAddWater.setLandscapeType("water");
-		map.setTile(tileToAddWater, 5, 5);
-		
-		tileToAddWater = map.getTile(2, 3);
-		tileToAddWater.setLandscapeType("water");
-		map.setTile(tileToAddWater, 2, 3);
+	}
+	
+	private void addSomeTrackTiles() {
+		for(int column = 0; column < 20; column++){
+		 map.getTile(4,column).setTrack(new Track());
+		}
 	}
 	
 	/* Setters and Getters */
