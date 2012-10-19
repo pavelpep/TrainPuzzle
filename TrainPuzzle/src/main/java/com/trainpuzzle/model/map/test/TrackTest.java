cdc3ce12a09d1d64dcac1c16bafac7f2e18487f4
pavelpep <p.pepeldjiyski@gmail.com>
2012-10-19 23:30:51 +0000

    created test for track
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@298 5d30610d-6562-4147-a3b2-4cfa0469b359

---
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/map/test/TrackTest.java
@@ -0,0 +1,91 @@
+package com.trainpuzzle.model.map.test;
+
+import junit.framework.TestCase;
+
+public class TrackTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testTrack() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testTrackConnection() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testTrackConnectionConnection() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testRotateTrack() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testAddConnection() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetConnections() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetOutboundHeading() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testNotifyAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testWaitLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testWaitLongInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testWait() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testFinalize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
