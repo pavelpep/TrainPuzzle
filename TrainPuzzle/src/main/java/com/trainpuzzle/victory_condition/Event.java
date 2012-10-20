e3a262ceb4046b1908d28c445b75ce55a3835765
dehud93 <hdeng@sfu.ca>
2012-10-20 00:01:29 +0000

    modify Event class
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@303 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/victory_condition/Event.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/victory_condition/Event.java
@@ -1,5 +1,31 @@
 package com.trainpuzzle.victory_condition;
 
-public interface Event {
+import com.trainpuzzle.model.map.Station;
 
+public class Event {
+	private int time;
+	private Station station;
+	
+	public Event(int time, Station station) {
+		super();
+		this.time = time;
+		this.station = station;
+	}
+	
+	public int getTime() {
+		return time;
+	}
+	
+	public void setTime(int time) {
+		this.time = time;
+	}
+	
+	public Station getStation() {
+		return station;
+	}
+	
+	public void setStation(Station station) {
+		this.station = station;
+	}
+	
 }
