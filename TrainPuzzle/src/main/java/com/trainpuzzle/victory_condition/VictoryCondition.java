d2f0111c671dd3e42dc9b20d1e374872267abf40
0ldskool <j.chahal20@gmail.com>
2012-10-20 02:31:38 +0000

    added some content to these originally empty class's. Pair programmed with James
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@309 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/victory_condition/VictoryCondition.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/victory_condition/VictoryCondition.java
@@ -1,5 +1,11 @@
 package com.trainpuzzle.victory_condition;
 
 public interface VictoryCondition {
+	public boolean isSatisfied();
+	public boolean isLeftSatisfied();
+	public boolean isRightSatisified();
+	
+	public void setLeftChild(VictoryCondition leftChild);
+	public void setRightChild(VictoryCondition rightChild);
 	
 }
--- TrainPuzzle/src/main/java/com/trainpuzzle/victory_condition/VictoryConditionEvaluator.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/victory_condition/VictoryConditionEvaluator.java
@@ -2,11 +2,16 @@ package com.trainpuzzle.victory_condition;
 
 public class VictoryConditionEvaluator {
 	private VictoryCondition root;
+	private boolean isSatisfied = false;
+	//private boolean canNeverBeSatisfied = false;
 
 	public VictoryConditionEvaluator(VictoryCondition root) {
 		this.root = root;
 	}
 	
+	public boolean isSatisfied() {
+		return isSatisfied;
+	}
 	
 	
 }
