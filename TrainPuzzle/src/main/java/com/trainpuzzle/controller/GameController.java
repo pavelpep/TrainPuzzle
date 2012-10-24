3ce0b44abe755f8c6e9f9723114f049f659dc7b9
rmkbow <ronald.bow@gmail.com>
2012-10-24 01:32:50 +0000

    observer for exceptions
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@409 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/controller/GameController.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/controller/GameController.java
@@ -2,19 +2,27 @@ package com.trainpuzzle.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.swing.JOptionPane;
 import javax.swing.Timer;
 
 import org.apache.log4j.Logger;
 
+import com.trainpuzzle.exception.CannotPlaceTrackException;
+import com.trainpuzzle.exception.CannotRemoveTrackException;
 import com.trainpuzzle.exception.TrainCrashException;
 import com.trainpuzzle.model.board.Board;
 import com.trainpuzzle.model.board.Track;
 import com.trainpuzzle.model.level.Level;
+import com.trainpuzzle.observe.Observable;
+import com.trainpuzzle.observe.Observer;
 import com.trainpuzzle.ui.windows.LoadedLevelScreen;
 
-public class GameController {
+public class GameController implements Observable{
 	private Logger logger = Logger.getLogger(Application.class);
+	private Set<Observer> observerList = new HashSet<Observer>();
 	
 	private CampaignManager campaignManager;
 	private TrackPlacer trackPlacer;
@@ -25,7 +33,7 @@ public class GameController {
 	private Level level;
 	private Level loadedLevelWithTrack;
 	
-	private boolean isTrainNotCrash;
+	private boolean trainCrashed;
 	
 	public GameController(){
 		
@@ -45,7 +53,7 @@ public class GameController {
 	}
 
 	public void runSimulation() {
-		isTrainNotCrash = true;
+		trainCrashed = false;
 	    ActionListener actionListener;
 	    
 	    
@@ -55,7 +63,7 @@ public class GameController {
 	         public void actionPerformed(ActionEvent actionEvent) {
 	        	move();
 	 			//uiLoadedLevel.redrawTrain(simulator.getTrain());
-	        	if(simulator.isVictoryConditionsSatisfied() || !isTrainNotCrash) {
+	        	if(simulator.isVictoryConditionsSatisfied() || trainCrashed) {
 	        		((Timer)actionEvent.getSource()).stop();
 	        	}
 	         }
@@ -66,7 +74,7 @@ public class GameController {
 	
 	public void resetSimulation() {
 		timer.stop();
-		isTrainNotCrash = true;
+		trainCrashed = false;
 	    simulator.reset();
 	    
 	}
@@ -75,8 +83,11 @@ public class GameController {
 			trackPlacer.placeTrack(track, row, column);
 			loadedLevelWithTrack = trackPlacer.getLevelWithTrack();
 			
-		} catch (Exception e) {
+		} catch (CannotPlaceTrackException e) {
 			logger.error(e.getMessage(), e.fillInStackTrace());
+			for(Observer observer : observerList) {
+				observer.notifyChange(e);
+			}
 		}
 	}
 	
@@ -84,8 +95,11 @@ public class GameController {
 		try {
 			trackPlacer.removeTrack(row, column);
 			loadedLevelWithTrack = trackPlacer.getLevelWithTrack();
-		} catch (Exception e) {
+		} catch (CannotRemoveTrackException e) {
 			logger.error(e.getMessage(), e.fillInStackTrace());
+			for(Observer observer : observerList) {
+				observer.notifyChange(e);
+			}
 		}
 	}
 	
@@ -94,7 +108,12 @@ public class GameController {
 			simulator.proceedNextTile();
 		} catch (TrainCrashException e) {
 			e.printStackTrace();
-			isTrainNotCrash = false;
+			trainCrashed = true;
+			//JOptionPane.showMessageDialog(null, "The train has crashed!");
+			for(Observer observer : observerList) {
+				observer.notifyChange(e);
+			}
+			
 		}
 	}
 	
@@ -115,4 +134,15 @@ public class GameController {
 	public Level getLevel() {
 		return this.level;
 	}
+	
+	public void register(Observer observer){
+		observerList.add(observer);
+	}
+	
+	public void notifyAllObservers(){
+		for(Observer observer : observerList) {
+			observer.notifyChange(this);
+		}
+	}
+	
 }
