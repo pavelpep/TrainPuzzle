85a4e4e6a3c971d993316fcb1a09b352be7c4b2b
rmkbow <ronald.bow@gmail.com>
2012-10-24 07:18:12 +0000

    new grass and water images
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@420 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/infrastructure/Images.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/infrastructure/Images.java
@@ -6,8 +6,8 @@ public final class Images {
 	
 
 	//String type
-	public final static String GRASS = "src/main/resources/images/grass.png";
-	public final static String WATER = "src/main/resources/images/water.png";
+	public final static String GRASS = "src/main/resources/images/grass2.png";
+	public final static String WATER = "src/main/resources/images/water2.png";
 	public final static String ROCK = "src/main/resources/images/rock.png";
 	public final static String TREES = "src/main/resources/images/trees.png";
 	
