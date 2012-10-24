39c7ca78b07c7ff739a0b9bd428f3d571dbc9e6c
dehud93 <hdeng@sfu.ca>
2012-10-24 00:40:51 +0000

    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@404 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/model/level/Level.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/level/Level.java
@@ -30,7 +30,7 @@ public class Level {
 	
 	private void initializeEmptyVictoryConditions() {
 		Station placeholderStation = new Station(StationType.GREEN, endLocation, CompassHeading.NORTH);
-		setVictoryConditions(new LeafVictoryCondition(new Event(1, placeholderStation)));
+		setVictoryConditions(new LeafVictoryCondition(new Event(1, placeholderStation,"")));
 	}
 	
 	/* 
