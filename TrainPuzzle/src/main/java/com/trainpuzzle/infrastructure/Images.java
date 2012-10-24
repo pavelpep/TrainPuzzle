073dc5d20134b3757d117bbe7bd815ef86e8799e
EternalFaith- <jyna.xii@gmail.com>
2012-10-24 20:22:44 +0000

    Formatting.
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@434 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/infrastructure/Images.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/infrastructure/Images.java
@@ -4,16 +4,18 @@ import javax.swing.ImageIcon;
 
 public final class Images {
 	
-
 	//String type
+	//Landscape and obstacles
 	public final static String GRASS = "src/main/resources/images/grass2.png";
 	public final static String WATER = "src/main/resources/images/water2.png";
 	public final static String ROCK = "src/main/resources/images/rock.png";
 	public final static String TREES = "src/main/resources/images/trees.png";
 	
+	//Stations
 	public final static String RED_STATION = "src/main/resources/images/redStation_Front.png";
 	public final static String GREEN_STATION = "src/main/resources/images/greenStation_Front.png";
 	
+	//Tracks
 	public final static String PERMANENT_STRAIGHT_TRACK = "src/main/resources/images/permanent_track.png";
 	public final static String STRAIGHT_TRACK = "src/main/resources/images/straight_track.png";
 	public final static String DIAGONAL_TRACK = "src/main/resources/images/diagonal_track.png";
@@ -23,14 +25,17 @@ public final class Images {
 	public final static String DIAGONAL_INTERSECTION_TRACK = "src/main/resources/images/diagonal_intersection_track.png";
 
 	//ImageIcon type
+	//Landscape and obstacles
 	public final static ImageIcon GRASS_IMAGE = new ImageIcon(GRASS);
 	public final static ImageIcon WATER_IMAGE = new ImageIcon(WATER);
 	public final static ImageIcon ROCK_IMAGE = new ImageIcon(ROCK);
 	public final static ImageIcon TREES_IMAGE = new ImageIcon(TREES);
 	
+	//Stations
 	public final static ImageIcon RED_STATION_IMAGE = new ImageIcon(RED_STATION);
 	public final static ImageIcon GREEN_STATION_IMAGE = new ImageIcon(GREEN_STATION);
 	
+	//Tracks
 	public final static ImageIcon PERMANENT_STRAIGHT_TRACK_IMAGE = new ImageIcon(PERMANENT_STRAIGHT_TRACK);
 	public final static ImageIcon STRAIGHT_TRACK_IMAGE = new ImageIcon(STRAIGHT_TRACK);
 	public final static ImageIcon DIAGONAL_TRACK_IMAGE = new ImageIcon( DIAGONAL_TRACK);
