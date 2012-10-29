1f56ad3084aa3337e5c25e4297ae80ebe4ebc67a
newmanlai <newmanlai.lm@gmail.com>
2012-10-29 23:27:50 +0000

    refactored and added more methods to the Switch class
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@456 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/model/board/Switch.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/board/Switch.java
@@ -3,18 +3,31 @@ import java.util.Iterator;
 
 public class Switch extends Track{
 	private Iterator<Connection> connectionsIterator;
-	private Connection currentConnection;
+	private Connection current;
 	
-	Switch(Connection headConnection, Connection tailConnection) {
-		super(headConnection, tailConnection);
-		currentConnection = headConnection;
+	Switch(Connection connection1, Connection connection2) {
+		super(connection1, connection2);
+		goToFirstConnection();
+	}
+	
+	private void goToFirstConnection() {
+		connectionsIterator = super.connections.iterator();
+		if (connectionsIterator.hasNext()) {
+			current = connectionsIterator.next();
+		} else {
+			// TODO: throw an assertion or exception while there should be 2 connections in the set
+		}
 	}
 	
 	public Connection getCurrentConnection() {
-		return currentConnection;
+		return current;
 	}
 	
 	public void switchConnection() {
-		// currentConnection = super.connections.iterator().next();
+		if (connectionsIterator.hasNext()) {
+			current = connectionsIterator.next();
+		} else {
+			goToFirstConnection();
+		}
 	}
 }
