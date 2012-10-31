284dfecd70aa1c960edce5bbddc52635db15099c
dehud93 <hdeng@sfu.ca>
2012-10-31 00:47:58 +0000

    fill the Economy class JJ James Frank Ronald
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@466 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/model/level/Economy.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/level/Economy.java
@@ -1,10 +1,44 @@
 package com.trainpuzzle.model.level;
 
+ import java.util.HashMap;
+
+import com.trainpuzzle.infrastructure.Images;
+
 public class Economy implements java.io.Serializable{
 
 	private static final long serialVersionUID = 1L;
-
+	public static final int NO_LIMIT = -1;
+	private int budget = 1000;
+	private int totalTrackLimit = 100;
+	private HashMap trackLimit = new HashMap();
+	private int singleTrackLimit = 20;
+	private int crossTrackLimit = 20;
 	public Economy(){
 		
+	}
+	public int getBudget() {
+		return budget;
+	}
+	public void setBudget(int budget) {
+		this.budget = budget;
+	}
+	public int getTotalTrackLimit() {
+		return totalTrackLimit;
+	}
+	public void setTotalTrackLimit(int totalTrackLimit) {
+		this.totalTrackLimit = totalTrackLimit;
+	}
+	
+	public int getSingleTrackLimit() {
+		return singleTrackLimit;
+	}
+	public void setSingleTrackLimit(int singleTrackLimit) {
+		this.singleTrackLimit = singleTrackLimit;
+	}
+	public int getCrossTrackLimit() {
+		return crossTrackLimit;
+	}
+	public void setCrossTrackLimit(int crossTrackLimit) {
+		this.crossTrackLimit = crossTrackLimit;
 	}
 }
