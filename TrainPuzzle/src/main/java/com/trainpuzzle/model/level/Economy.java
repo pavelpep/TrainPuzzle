5eeeda9b83f4d61a11de86f97b60a828eb19fb77
frank-guo <frank_guoguo@hotmail.com>
2012-10-31 08:58:34 +0000

    add trackCouter and relevant methods in economy and write constructor of economy
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@469 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/model/level/Economy.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/level/Economy.java
@@ -1,6 +1,8 @@
 package com.trainpuzzle.model.level;
 
- import java.util.HashMap;
+import java.util.HashMap;
+
+import com.trainpuzzle.model.board.*;
 
 import com.trainpuzzle.infrastructure.Images;
 
@@ -8,14 +10,33 @@ public class Economy implements java.io.Serializable{
 
 	private static final long serialVersionUID = 1L;
 	public static final int NO_LIMIT = -1;
+	private static final int individualTrackLimit = 10;
 	private int budget = 1000;
 	private int totalTrackLimit = 100;
 	private HashMap trackLimit = new HashMap();
+	private HashMap trackCounter = new HashMap();
 	private int singleTrackLimit = 20;
 	private int crossTrackLimit = 20;
+	
 	public Economy(){
+		for(TrackType trackType:TrackType.values()){
+			trackLimit.put(trackType, individualTrackLimit);
+			trackCounter.put(trackType, 0);
+			
+		}
+	}
+	
+	public void incrementTrackCounter(TrackType trackType){
+		Object currentNumOfTheTypeOfTrack=trackCounter.get(trackType);
+		
 		
 	}
+	
+	public void decreaseTrackCounter(TrackType trackType){
+		Object currentNumOfTheTypeOfTrack=trackCounter.get(trackType);
+		
+	}
+	
 	public int getBudget() {
 		return budget;
 	}
