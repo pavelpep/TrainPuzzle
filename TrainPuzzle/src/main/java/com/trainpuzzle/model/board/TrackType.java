052e3d7b0beda1b17237aef02251db4a1fec5b96
frank-guo <frank_guoguo@hotmail.com>
2012-11-02 01:17:05 +0000

    Refactor TrackType, using enum tree to accomplish the track limits of three levels
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@480 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/model/board/TrackType.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/model/board/TrackType.java
@@ -1,12 +1,46 @@
 package com.trainpuzzle.model.board;
 
+import java.util.HashMap;
+
 public enum TrackType {
-	STRAIGHT_TRACK,
-	CURVELEFT_TRACK,
-	CURVERIGHT_TRACK,
-	DIAGONAL_TRACK,
-	INTERSECTION_TRACK,
-	DIAGONAL_INTERSECTION_TRACK;
+	TRACK(null,60),
+		STRAIGHT(TRACK, 20),
+			STRAIGHT_TRACK(STRAIGHT, 15),
+			DIAGONAL_TRACK(STRAIGHT,15),
+		CURVE(TRACK, 20),
+			CURVELEFT_TRACK(CURVE,15),
+			CURVERIGHT_TRACK(CURVE,15),
+		INTERSECTION(TRACK, 15),
+			INTERSECTION_TRACK(INTERSECTION, 10),
+			DIAGONAL_INTERSECTION_TRACK(INTERSECTION,10);
+
+	private TrackType parent = null;
+	private int currentTrackLimit = 0;
+	private TrackType(TrackType parent, Integer trackLimit){
+		this.parent = parent;
+		this.currentTrackLimit = trackLimit;
+		}
+	
+		
+	 public int getTrackLimit(TrackType trackType){
+		 return trackType.currentTrackLimit;
+	 }
+	 
+	 public void incrementTrackLimit(TrackType trackType){
+		 trackType.currentTrackLimit++;
+		 TrackType parent = trackType.parent;
+		 TrackType grandparent = parent.parent;
+		 parent.currentTrackLimit++;
+		 grandparent.currentTrackLimit++;
+	 }
+	 
+	 public void decrementTrackLimit(TrackType trackType){
+		 trackType.currentTrackLimit--;
+		 TrackType parent = trackType.parent;
+		 TrackType grandparent = parent.parent;
+		 parent.currentTrackLimit--;
+		 grandparent.currentTrackLimit--;
+	 }	 
 	
 	
 }
