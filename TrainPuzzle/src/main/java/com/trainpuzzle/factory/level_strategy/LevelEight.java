1f6d9e7aaec86499dd4d4cd62ac3eed64140c4f9
newmanlai <newmanlai.lm@gmail.com>
2012-11-26 04:09:56 +0000

    Completed level eight
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@754 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/factory/level_strategy/LevelEight.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/factory/level_strategy/LevelEight.java
@@ -18,43 +18,9 @@ import com.trainpuzzle.model.level.victory_condition.AndVictoryCondition;
 
 public class LevelEight extends LevelOutline {
 
-	/*
-	private void setObstacles() {
-		
-		setObstaclesByRow(2, 0, 2, ObstacleType.MOUNTAINS);
-		setObstaclesByRow(3, 0, 1, ObstacleType.MOUNTAINS);
-		
-		setObstaclesByRow(9, 1, 1, ObstacleType.MOUNTAINS);
-		setObstaclesByRow(10, 1, 4, ObstacleType.MOUNTAINS);
-		setObstaclesByColumn(7, 7, 4, ObstacleType.TREES);
-		
-		setObstaclesByColumn(0, 5, 4, ObstacleType.MOUNTAINS);
-		setObstaclesByColumn(5, 6, 5, ObstacleType.MOUNTAINS);
-		
-		setObstaclesByColumn(9, 9, 5, ObstacleType.TREES);
-		
-		setObstaclesByColumn(6, 8, 6, ObstacleType.MOUNTAINS);
-		setObstaclesByColumn(10, 14, 6, ObstacleType.MOUNTAINS);
-		setObstaclesByColumn(10, 14, 7, ObstacleType.MOUNTAINS);
-		
-		setObstaclesByColumn(3, 8, 8, ObstacleType.MOUNTAINS);
-		
-		setObstaclesByRow(3, 11, 14, ObstacleType.MOUNTAINS);
-		setObstaclesByRow(7, 11, 11, ObstacleType.TREES);
-		 
-        
-        ArrayList<Location> rockLocations = new ArrayList<Location>();
-        rockLocations.add(new Location(4, 5));
-        rockLocations.add(new Location(3, 2));
-        setObstacles(rockLocations, ObstacleType.ROCK);
-        
-	}
-	
-	*/
-	
 	@Override
 	public Level createLevel() {
-		this.board = new Board(11, 15);
+		this.board = new Board(11, 13);
         this.root = new AndVictoryCondition();
         
         initStations();
@@ -80,46 +46,69 @@ public class LevelEight extends LevelOutline {
 
 	private void initStations() {
 		ArrayList<Station> stations = new ArrayList<Station>();
-        stations.add(new Station(StationType.RED, new Location(4, 14), CompassHeading.SOUTH));
+        stations.add(new Station(StationType.RED, new Location(4, 12), CompassHeading.SOUTH));
         
         setStations(stations);
 	}
 
 	private HashMap<TrackType, Integer> createTrackLimits() {        
 		HashMap<TrackType, Integer> trackLimits = new HashMap<TrackType,Integer>();
-        trackLimits.put(TrackType.TRACK, 15);
-        trackLimits.put(TrackType.STRAIGHT, 5);
-        trackLimits.put(TrackType.CURVE, 5);
-        trackLimits.put(TrackType.INTERSECTION, 1);
-        trackLimits.put(TrackType.SWITCH, 1);
-        trackLimits.put(TrackType.STRAIGHT_TRACK, 5);
-        trackLimits.put(TrackType.DIAGONAL_TRACK, 5);
-        trackLimits.put(TrackType.CURVELEFT_TRACK, 5);
-        trackLimits.put(TrackType.CURVERIGHT_TRACK, 5);
-        trackLimits.put(TrackType.INTERSECTION_TRACK, 1);
-        trackLimits.put(TrackType.DIAGONAL_INTERSECTION_TRACK, 1);
-        trackLimits.put(TrackType.CURVELEFT_STRAIGHT_SWITCH, 1);
-        trackLimits.put(TrackType.CURVERIGHT_STRAIGHT_SWITCH, 1);
+        trackLimits.put(TrackType.TRACK, 25);
+        trackLimits.put(TrackType.STRAIGHT, 20);
+        trackLimits.put(TrackType.CURVE, 20);
+        trackLimits.put(TrackType.INTERSECTION, 3);
+        trackLimits.put(TrackType.SWITCH, 3);
+        trackLimits.put(TrackType.STRAIGHT_TRACK, 12);
+        trackLimits.put(TrackType.DIAGONAL_TRACK, 12);
+        trackLimits.put(TrackType.CURVELEFT_TRACK, 12);
+        trackLimits.put(TrackType.CURVERIGHT_TRACK, 12);
+        trackLimits.put(TrackType.INTERSECTION_TRACK, 2);
+        trackLimits.put(TrackType.DIAGONAL_INTERSECTION_TRACK, 2);
+        trackLimits.put(TrackType.CURVELEFT_STRAIGHT_SWITCH, 2);
+        trackLimits.put(TrackType.CURVERIGHT_STRAIGHT_SWITCH, 2);
         
         return trackLimits;
 	}
 
 	private void setObstacles() {
+		ObstacleType trees = ObstacleType.TREES;
 		
-		setObstaclesByColumn(2, 3, 6, ObstacleType.TREES);
+		setObstaclesByColumn(2, 4, 3, trees);
+		setObstaclesByColumn(6, 7, 3, trees);
+		setObstaclesByColumn(7, 8, 5, trees);
+		setObstaclesByColumn(3, 4, 7, trees);
+		setObstaclesByColumn(7, 8, 7, trees);
+		setObstaclesByColumn(5, 7, 8, trees);
+		setObstaclesByColumn(2, 4, 9, trees);
+		setObstaclesByColumn(2, 3, 11, trees);
 		
-		
-		setObstaclesByRow(1, 4, 9, ObstacleType.TREES);
-		setObstaclesByRow(9, 11, 9, ObstacleType.TREES);
-		setObstaclesByRow(1, 4, 10, ObstacleType.TREES);
-		setObstaclesByRow(6, 7, 10, ObstacleType.TREES);
+		setObstaclesByRow(1, 7, 10, trees);
+		setObstaclesByRow(2, 5, 6, trees);
+		setObstaclesByRow(5, 4, 6, trees);
+		setObstaclesByRow(9, 0, 3, trees);
+		setObstaclesByRow(9, 8, 10, trees);
+		setObstaclesByRow(10, 0, 3, trees);
+		setObstaclesByRow(10, 5, 6, trees);
         
-        /*
-        ArrayList<Location> rockLocations = new ArrayList<Location>();
-        rockLocations.add(new Location(4, 5));
-        rockLocations.add(new Location(3, 2));
-        setObstacles(rockLocations, ObstacleType.ROCK);
-        */
+        
+        ArrayList<Location> treesLocations = new ArrayList<Location>();
+        treesLocations.add(new Location(0, 1));
+        treesLocations.add(new Location(0, 4));
+        treesLocations.add(new Location(0, 6));
+        treesLocations.add(new Location(0, 11));
+        treesLocations.add(new Location(2, 2));
+        treesLocations.add(new Location(2, 8));
+        treesLocations.add(new Location(3, 4));
+        treesLocations.add(new Location(4, 5));
+        treesLocations.add(new Location(5, 10));
+        treesLocations.add(new Location(6, 4));
+        treesLocations.add(new Location(6, 9));
+        treesLocations.add(new Location(7, 11));
+        treesLocations.add(new Location(8, 1));
+        treesLocations.add(new Location(8, 10));
+        treesLocations.add(new Location(9, 6));
+        setObstacles(treesLocations, trees);
+        
 	}
 
 }
