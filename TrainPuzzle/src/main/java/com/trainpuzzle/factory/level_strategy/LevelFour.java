56970c87e6b143574bf3e024d40c7c65eacf46db
shannaw <shanna.walters@gmail.com>
2012-11-27 20:38:13 +0000

    Level 4 Updated.
    git-svn-id: https://punch.csil.sfu.ca/svn/CMPT373-2012-omega@784 5d30610d-6562-4147-a3b2-4cfa0469b359

--- TrainPuzzle/src/main/java/com/trainpuzzle/factory/level_strategy/LevelFour.java
+++ TrainPuzzle/src/main/java/com/trainpuzzle/factory/level_strategy/LevelFour.java
@@ -24,7 +24,7 @@ public class LevelFour extends LevelOutline {
 	
 	@Override
 	public Level createLevel() {
-        this.board = new Board(12, 12, LandscapeType.LIGHTDIRT);
+        this.board = new Board(12, 13, LandscapeType.LIGHTDIRT);
 		this.root = new AndVictoryCondition();
 		 root.setDefaultModel();
 
@@ -45,10 +45,11 @@ public class LevelFour extends LevelOutline {
 	}
 
 	private Location createStartLocation() {
-		Location startLocation = new Location(0, 8);
-        setStartLocation(startLocation, CompassHeading.WEST, CompassHeading.EAST, TrackType.STRAIGHT_TRACK);
+		Location startLocation = new Location(1, 3);
+        setStartLocation(startLocation, CompassHeading.WEST, CompassHeading.EAST, TrackType.STRAIGHT);
 		return startLocation;
 	}
+
 
 	private void initStations() {
 		ArrayList<Station> stations = new ArrayList<Station>();
@@ -57,12 +58,12 @@ public class LevelFour extends LevelOutline {
         stationToAdd.addExportCargo(new Cargo(CargoType.IRON));
         stationToAdd.addExportCargo(new Cargo(CargoType.WOOD));
         
-        stationToAdd = new Station(StationType.RED, new Location(0, 3), CompassHeading.SOUTH);
+        stationToAdd = new Station(StationType.RED, new Location(7, 3), CompassHeading.SOUTH);
         stations.add(stationToAdd);	        
         addImportCargo (stationToAdd, root, new Cargo(CargoType.IRON));
         stationToAdd.addExportCargo(new Cargo(CargoType.COTTON));
         
-        stationToAdd = new Station(StationType.GREEN, new Location(5, 7), CompassHeading.EAST);
+        stationToAdd = new Station(StationType.GREEN, new Location(6, 7), CompassHeading.EAST);
         stations.add(stationToAdd);	        
         addImportCargo (stationToAdd, root, new Cargo(CargoType.COTTON));
         addImportCargo (stationToAdd, root, new Cargo(CargoType.WOOD));
@@ -72,19 +73,19 @@ public class LevelFour extends LevelOutline {
 
 	private HashMap<TrackType, Integer> createTrackLimits() {        
 		HashMap<TrackType, Integer> trackLimits = new HashMap<TrackType,Integer>();
-        trackLimits.put(TrackType.TRACK, 60);
-        trackLimits.put(TrackType.STRAIGHT, 18);
-        trackLimits.put(TrackType.CURVE, 38);
+        trackLimits.put(TrackType.TRACK, 107);
+        trackLimits.put(TrackType.STRAIGHT, 35);
+        trackLimits.put(TrackType.CURVE, 30);
         trackLimits.put(TrackType.INTERSECTION, 2);
-        trackLimits.put(TrackType.SWITCH, 2);
-        trackLimits.put(TrackType.STRAIGHT_TRACK, 18);
-        trackLimits.put(TrackType.DIAGONAL_TRACK, 18);
-        trackLimits.put(TrackType.CURVELEFT_TRACK, 19);
-        trackLimits.put(TrackType.CURVERIGHT_TRACK, 19);
+        trackLimits.put(TrackType.SWITCH, 10);
+        trackLimits.put(TrackType.STRAIGHT_TRACK, 25);
+        trackLimits.put(TrackType.DIAGONAL_TRACK, 10);
+        trackLimits.put(TrackType.CURVELEFT_TRACK, 15);
+        trackLimits.put(TrackType.CURVERIGHT_TRACK, 15);
         trackLimits.put(TrackType.INTERSECTION_TRACK, 2);
         trackLimits.put(TrackType.DIAGONAL_INTERSECTION_TRACK, 2);
-        trackLimits.put(TrackType.CURVELEFT_STRAIGHT_SWITCH,2);
-        trackLimits.put(TrackType.CURVERIGHT_STRAIGHT_SWITCH, 2);
+        trackLimits.put(TrackType.CURVELEFT_STRAIGHT_SWITCH,5);
+        trackLimits.put(TrackType.CURVERIGHT_STRAIGHT_SWITCH, 5);
 		return trackLimits;
 	}
 
@@ -101,23 +102,47 @@ public class LevelFour extends LevelOutline {
 		setLandscapeByRow(3, 6, 9, LandscapeType.DIRT);
 		setLandscapeByRow(7, 6, 9, LandscapeType.DIRT);
 		setLandscapeByRow(8, 6, 9, LandscapeType.DIRT);
+		
+		setLandscapeByColumn(0, 7, 9, LandscapeType.ROUGHDIRT);
+		setLandscapeByColumn(1, 7, 8, LandscapeType.ROUGHDIRT);
+		
+		setLandscapeByRow(2, 0, 1, LandscapeType.WATER);
+		setLandscapeByRow(3, 0, 4, LandscapeType.WATER);
+		setLandscapeByRow(4, 0, 5, LandscapeType.WATER);
+		setLandscapeByRow(5, 1, 5, LandscapeType.WATER);
+		setLandscapeByRow(6, 2, 6, LandscapeType.WATER);
+		setLandscapeByRow(9, 4, 6, LandscapeType.WATER);
 	}
 
+
 	private void setObstacles() {
-		ArrayList<Location> mountainLocations = new ArrayList<Location>();
-        mountainLocations.add(new Location(8, 10));
-        mountainLocations.add(new Location(9, 7));
-        setObstacles(mountainLocations, ObstacleType.MOUNTAINS);
-        
-        setObstaclesByRow(8, 9, 11, ObstacleType.MOUNTAINS);
-        setObstaclesByRow(9, 10, 11, ObstacleType.MOUNTAINS);
-        setObstaclesByRow(10, 11, 11, ObstacleType.MOUNTAINS);
+        setObstaclesByRow(0, 0, 7, ObstacleType.MOUNTAINS);
+        setObstaclesByRow(1, 0, 2, ObstacleType.MOUNTAINS);
+        setObstaclesByRow(1, 5, 8, ObstacleType.MOUNTAINS);
+        setObstaclesByRow(2, 2, 3, ObstacleType.MOUNTAINS);
         
 		ArrayList<Location> treeLocations = new ArrayList<Location>();
+		treeLocations.add(new Location(5, 0));
+		treeLocations.add(new Location(6, 0));
         treeLocations.add(new Location(5, 6));
         treeLocations.add(new Location(2, 6));
+        treeLocations.add(new Location(2, 7));
+        treeLocations.add(new Location(3, 7));
+        treeLocations.add(new Location(4, 11));
+        treeLocations.add(new Location(4, 9));
+        treeLocations.add(new Location(4, 10));
         treeLocations.add(new Location(6, 9));
+        treeLocations.add(new Location(9, 2));
+        treeLocations.add(new Location(9, 7));
+        treeLocations.add(new Location(9, 9));
         setObstacles(treeLocations, ObstacleType.TREES);
+        
+        ArrayList<Location> rockLocations = new ArrayList<Location>();
+        rockLocations.add(new Location(6, 1));
+        rockLocations.add(new Location(9, 3));
+        rockLocations.add(new Location(9, 8));
+        rockLocations.add(new Location(9, 10));
+        setObstacles(rockLocations, ObstacleType.ROCK);
 	}
 
 }
